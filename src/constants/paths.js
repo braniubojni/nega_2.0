98cb903076405a3ed4c9d37966d63bd58de47423
admin <Erik>
2021-10-03 12:37:01 +0400

    added constants

---
+++ src/constants/paths.js
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = "/";
+export const SIGN_IN_ROUTE = "/sign-in";
+export const SIGN_UP_ROUTE = "/sign-up";
+export const CHANNELS_ROUTE = "/channels";
+export const PERSONALROOM_ROUTE = "/personal-room";
