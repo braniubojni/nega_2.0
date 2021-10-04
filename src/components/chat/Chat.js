8d5a90eef4188c04236b095df16aa341aede067b
admin <Erik>
2021-10-04 22:41:54 +0400

    added writing a chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,12 +1,24 @@
 import { getAuth } from "@firebase/auth";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
-import { collection, doc, addDoc, serverTimestamp } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import styled from "styled-components";
 import db from "../../firebase";
 import {
   selectChannelId,
   selectChannelName,
 } from "../../redux/common/channel/selectors";
+
+const BottomDiv = styled.div`
+  padding-bottom: 20px;
+`;
+const MainContentWrapper = styled.div`
+  position: relative;
+`;
+const H4 = styled.div`
+  position: absolute;
+  top: 20px;
+`;
 
 function Chat() {
   const channelId = useSelector(selectChannelId);
@@ -14,30 +26,38 @@ function Chat() {
   const inputRef = useRef("");
   const chatRef = useRef(null);
 
-  const scrollToBottom = () => {};
+  const scrollToBottom = () => {
+    chatRef.current.scrollInfoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
 
   const sendMessage = async (evn) => {
     evn.preventDefault();
     const auth = getAuth();
     if (inputRef.current.value.trim()) {
-      const collectionRef = collection(db, `channels/${channelId}/messages`);
-      const payload = {
-        timestamp: "timestampFromServer",
-        message: "someMessage",
-        name: "userName",
-      };
-      // inputRef.current.value = "";
-      // scrollToBottom();
+      const docRef = await addDoc(
+        collection(db, `channels/${channelId}/messages`),
+        {
+          timestamp: serverTimestamp(),
+          message: inputRef.current.value,
+          name: auth.currentUser.email,
+        }
+      );
+      inputRef.current.value = "";
+      scrollToBottom();
     }
   };
   return (
     <div>
-      <h4>
+      <H4>
         {!channelName
           ? "Select any channel"
           : `You are in the channel ${channelName}`}
-      </h4>
+      </H4>
       <ul>{}</ul>
+      <BottomDiv ref={chatRef} />
       <form>
         <input
           type="text"
