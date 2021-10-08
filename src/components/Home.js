6c15b7b82f0ae37a2af9c218cee8e636098109df
braniubojni <braniubojni@mail.ru>
2021-10-08 23:57:28 +0400

    Added header Gohar will continue

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { CHANNELS_ROUTE, SIGN_IN_ROUTE } from "../constants/paths";
 import { selectLoggedInUser } from "../redux/common/auth/selectors";
+import Appbar from "../components/header/Appbar";
+import AppbarDrawer from "../components/header/AppbarDrawer";
+import { Toolbar } from "@mui/material";
 
 // yst redux -i kimananq te SignIn exaca te piti lini
 // ev yst dran kimananq te renderi
@@ -12,17 +15,7 @@ function Home() {
 
   return (
     <header style={{ display: "flex" }}>
-      <p>Icon</p>
-      <h1>I am header</h1>
-      <button
-        onClick={() =>
-          !loggedUser
-            ? history.push(SIGN_IN_ROUTE)
-            : history.push(CHANNELS_ROUTE)
-        }
-      >
-        {!loggedUser ? "Try for free" : "Open channels"}
-      </button>
+      <AppbarDrawer />
     </header>
   );
 }
