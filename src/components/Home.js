4a8c9f980b6bc473b1d1f268fd9627f1a37be1eb
braniubojni <braniubojni@mail.ru>
2021-10-09 20:21:21 +0400

    fix some bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
-import ChannelArea from "./header/ChannelArea";
+import { useHistory } from "react-router";
+import { CHANNELS_ROUTE, SIGN_IN_ROUTE } from "../constants/paths";
 
 function Home() {
+  const history = useHistory();
   return (
     <div>
-      <ChannelArea />
+      <h1>This should be the home page</h1>
+      <button onClick={history.push(CHANNELS_ROUTE)}>To channels</button>
+      <button onClick={history.push(SIGN_IN_ROUTE)}>To Sign in</button>
     </div>
   );
 }
