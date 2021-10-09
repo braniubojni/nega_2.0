c4f38debdc25274acce5537855c42121f57e0855
Gohar <gohar.mamunts@gmail.com>
2021-10-09 20:46:14 +0400

    added basic Navbar parts

---
+++ src/components/header/Drawer.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import Logo from "Logo/logo.svg";
+import SmallDropdown from "./SmallDropdown";
+
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  IconButton,
+  Typography,
+  Box,
+} from "@mui/material";
+
+function DrawerComponent() {
+  const [openDrawer, setOpenDrawer] = useState(true);
+
+  const handleDrawerToggling = () => {
+    setOpenDrawer(false);
+  };
+
+  const menuItems = ["Product", "Enterprise", "Recources", "Pricing"];
+
+  return (
+    <>
+      <Drawer open={openDrawer} anchor="left" onClose={handleDrawerToggling}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <Box>
+            <Typography>
+              <img src={Logo} width="100px"></img>
+            </Typography>
+          </Box>
+          <Box>
+            <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        </Box>
+
+        <List sx={{ minWidth: 500 }}>
+          {menuItems.map((item, index) => {
+            return index === 0 ? (
+              <SmallDropdown />
+            ) : (
+              <ListItem>
+                <ListItemIcon key={item}>
+                  <ListItemText>{item}</ListItemText>
+                </ListItemIcon>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Drawer>
+
+      <Box
+        sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}
+      >
+        <Box>
+          <Typography>
+            <img src={Logo} width="100px"></img>
+          </Typography>
+        </Box>
+        <Box>
+          <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+            <MenuIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default DrawerComponent;
