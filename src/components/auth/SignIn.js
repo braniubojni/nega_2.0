0bdc6a35f2b339cf5a4af621596dec7a9f64fa7f
Artur <tov.art@bk.ru>
2021-10-10 12:57:52 +0400

    sigin Style

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,13 +2,27 @@ import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import db from "../../firebase";
-import { collection, onSnapshot } from "@firebase/firestore";
+import { collection, limit, onSnapshot } from "@firebase/firestore";
 import { setLoggedinUser } from "../../redux/common/auth/actions";
 import { CHANNELS_ROUTE, SIGN_UP_ROUTE } from "../../constants/paths";
 import { signInWithEmailAndPassword, getAuth } from "@firebase/auth";
 import { selectLoggedInUser } from "../../redux/common/auth/selectors";
 import Alert from "../dialogs/Alert";
 import Loader from "../loader/Loader";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import {
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { FlexContainer, ImgContainer, ImgLogo } from "./SignInStyle";
+import logo from "../../../src/image/slack_logo.svg";
 
 function SignIn() {
   const history = useHistory();
@@ -19,7 +33,13 @@ function SignIn() {
   const loggedInUser = useSelector(selectLoggedInUser);
   const [alert, setAlert] = useState(false);
   const [loader, setLoader] = useState(false);
-
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const handleChangeUserEmail = (e) => setUsrEmail(e.target.value);
+  const handleChangeUserPassword = (e) => setUsrPassword(e.target.value);
   useEffect(() => {
     if (loggedInUser) {
       history.push(CHANNELS_ROUTE);
@@ -58,32 +78,77 @@ function SignIn() {
         <Loader loader={loader} />
       ) : (
         <>
-          <h1>On Sign In</h1>
-          <form onSubmit={handleExistingUser}>
-            <label>
-              Email:
-              <input
-                type="text"
-                value={usrEmail}
-                onChange={(e) => setUsrEmail(e.target.value)}
-              />
-            </label>
-            <label>
-              Password:
-              <input
-                type="text"
-                value={usrPassword}
-                onChange={(e) => setUsrPassword(e.target.value)}
-              />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-          <button onClick={() => history.push(SIGN_UP_ROUTE)}>Sign up</button>
-          {loggedInUser && (
-            <button onClick={() => history.push(CHANNELS_ROUTE)}>
-              to channels
-            </button>
-          )}
+          <Container maxWidth="sm">
+            <FlexContainer>
+              <ImgContainer>
+                <ImgLogo src={logo} />
+              </ImgContainer>
+              <form onSubmit={handleExistingUser}>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                  value={usrEmail}
+                  onChange={handleChangeUserEmail}
+                  type="email"
+                />
+                <FormControl fullWidth variant="outlined" margin="normal">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    value={usrPassword}
+                    onChange={handleChangeUserPassword}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? (
+                            <VisibilityOffIcon />
+                          ) : (
+                            <VisibilityIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+              </form>
+              <Button
+                variant="outlined"
+                fullWidth
+                sx={{
+                  marginTop: 2,
+                  height: 50,
+                }}
+                onClick={() => history.push(SIGN_UP_ROUTE)}
+              >
+                Sign up
+              </Button>
+              {loggedInUser && (
+                <Button
+                  varinat="outlined"
+                  fullWidth
+                  sx={{
+                    marginTop: 2,
+                    height: 50,
+                  }}
+                  onClick={() => history.push(CHANNELS_ROUTE)}
+                >
+                  to channels
+                </Button>
+              )}
+            </FlexContainer>
+          </Container>
 
           {alert && <Alert alert={alert} setAlert={setAlert} />}
         </>
