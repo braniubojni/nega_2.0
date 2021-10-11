66dc787012e857c3ff4a8a7fc5fcf977b81e20fe
braniubojni <braniubojni@mail.ru>
2021-10-11 22:52:13 +0400

    emoji position fixed

--- src/components/channels/Channel.js
+++ src/components/channels/Channel.js
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { setChannelInfo } from "../../redux/common/channel/actions";
 import { CHANNELS_ROUTE } from "../../constants/paths";
-import { ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { ListItemButton, ListItemText } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 import { Box } from "@mui/system";
