4ef69fa3aafa4be0ef09ab56164373a6d7328adc
Artur <tov.art@bk.ru>
2021-10-12 17:05:03 +0400

    contents Change

--- src/components/main/Conntent.js
+++ src/components/main/Conntent.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
-import { Box, fontSize, styled } from "@mui/system";
+import { styled } from "@mui/system";
 import { Container, Typography } from "@mui/material";
-import YouTubeVideo from "./YouTube";
 import DialogVideo from "./VideoDialogs";
 import useWindowResize from "../helpers/customHooks/useWindowResize";
-import { faAlignCenter } from "@fortawesome/free-solid-svg-icons";
 
 const Content = styled("div")(({ theme }) => ({
-  marginTop: 50,
+  marginTop: 60,
   display: "flex",
   justifyContent: "space-between",
   //   alignItems: "center",
   paddingLeft: 15,
   paddingRight: 15,
   flexDirection: "row",
-  marginTop: 40,
+  color: "#1d1d1d",
   [theme.breakpoints.down("1000")]: {
+    marginTop: 40,
     justifyContent: "center",
   },
 }));
@@ -23,28 +22,30 @@ const H2 = styled("h2")(({ theme }) => ({
   fontSize: "2rem",
   fontWeight: 900,
 }));
-const Text = styled("span")(({ theme }) => ({
-  lineHeight: 1.44444444,
-  letterSpacing: -0.2,
-  fontSize: "1.125rem",
-}));
-const ContentItemWraper = styled("div")(({ theme }) => ({}));
-const ContentContainer = styled("div")(({ theme }) => ({
+
+const ContentItemWraper = styled("div")(({ theme }) => ({
+  width: 500,
   display: "flex",
   justifyContent: "space-between",
   flexDirection: "column",
+  flexWrap: "wrap",
 }));
+const ContentImgItem = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  [theme.breakpoints.down("501")]: {
+    flexWrap: "wrap",
+    marginTop: 15,
+  },
+}));
+
 const ContentImgConTainer = styled("div")(({ theme }) => ({
   display: "flex",
   justifyContent: "space-between",
   [theme.breakpoints.down(600)]: {},
-  [theme.breakpoints.down(400)]: {
-    marginTop: 15,
-    flexWrap: "wrap",
-  },
 }));
 const Video = styled("video")({
-  minWidth: 200,
+  minWidth: 400,
   width: "125%",
 });
 const Img = styled("img")(({ theme }) => ({
@@ -77,23 +78,22 @@ function Contents({ content, index }) {
             flexDirection: index % 2 === 0 ? "row" : "row-reverse",
           }}
         >
-          <Box
-            sx={{
-              maxWidth: 500,
-            }}
-          >
+          <ContentItemWraper>
             <H2>{content.title}</H2>
-            <Typography variant="subtitle1" component="div">
-              {content.text}
-            </Typography>
-            <Box
+            <Typography
+              variant="subtitle1"
+              component="div"
               sx={{
-                display: "flex",
-                justifyContent: "space-between",
+                lineHeight: 1.44444444,
+                letterSpacing: -0.2,
+                fontSize: "1.125rem",
               }}
             >
+              {content.text}
+            </Typography>
+            <ContentImgItem>
               {content.img.map((item, index) => (
-                <div>
+                <div key={content.imgName[index]}>
                   <Img src={item} onClick={handleOpen} />
                   {open && (
                     <DialogVideo
@@ -104,8 +104,8 @@ function Contents({ content, index }) {
                   )}
                 </div>
               ))}
-            </Box>
-          </Box>
+            </ContentImgItem>
+          </ContentItemWraper>
           <ContentImgConTainer
             style={
               index % 2 === 0
