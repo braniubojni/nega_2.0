6ee4a317b9509aee545811be8f3b61b1c17e002c
Erik Harutyunyan <erik95harutyunyan@gmail.com>
2021-10-12 13:13:31 +0400

    applyMiddleware(thunk) not working for now

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ const RootReducer = (state, action) => {
 
 const store = createStore(
   RootReducer,
-  applyMiddleware(thunk),
+
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
