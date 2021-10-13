ebba67b5b427c3605bfce55bcc8ee1392a099ac2
Artur <tov.art@bk.ru>
2021-10-13 14:15:49 +0400

    navbar change

--- src/components/homepage/navbar/Navbar.js
+++ src/components/homepage/navbar/Navbar.js
@@ -8,6 +8,7 @@ import {
   Button,
   useMediaQuery,
   useTheme,
+  Container,
 } from "@mui/material";
 
 import Dropdown from "./Dropdown";
@@ -15,8 +16,16 @@ import DrawerComponent from "./Drawer";
 import Logo from "../../../logo/logo.svg";
 import { Link } from "react-router-dom";
 import { SIGN_IN_ROUTE, SIGN_UP_ROUTE } from "../../../constants/paths";
-
+import { styled } from "@mui/system";
+import useWindowResize from "../../helpers/customHooks/useWindowResize";
+const FlexCOntainer = styled("div")(({ theme }) => ({
+  width: 1220,
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
 function Navbar() {
+  const windowWidth = useWindowResize();
   const [value, setValue] = useState(0);
 
   const handleClickTab = (evt, newValue) => {
@@ -28,35 +37,41 @@ function Navbar() {
 
   return (
     <>
-      <AppBar color="secondary">
-        <Toolbar>
-          {isSmallScreen ? (
-            <DrawerComponent />
-          ) : (
-            <>
-              <Typography>
-                <img src={Logo} width="100px" alt="slack_logo" />
-              </Typography>
-              <Tabs
-                onChange={handleClickTab}
-                indicatorColor="primary"
-                value={value}
-              >
-                <Tab disableRipple label={<Dropdown />} />
-                <Tab disableRipple label="Enterprise" />
-                <Tab disableRipple label="Recources" />
-                <Tab disableRipple label="Pricing" />
-              </Tabs>
-              <Button>
-                <Link to={SIGN_UP_ROUTE}>SIGN UP</Link>
-              </Button>
-              <Button>
-                <Link to={SIGN_IN_ROUTE}>Try for free</Link>
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
+      <Container maxWidth="lg">
+        <AppBar color="secondary">
+          <Toolbar>
+            {isSmallScreen ? (
+              <DrawerComponent />
+            ) : (
+              <>
+                <FlexCOntainer>
+                  <Typography>
+                    <img src={Logo} width="100px" alt="slack_logo" />
+                  </Typography>
+                  <Tabs
+                    onChange={handleClickTab}
+                    indicatorColor="primary"
+                    value={value}
+                  >
+                    <Tab disableRipple label={<Dropdown />} />
+                    <Tab disableRipple label="Enterprise" />
+                    <Tab disableRipple label="Recources" />
+                    <Tab disableRipple label="Pricing" />
+                  </Tabs>
+                  <div>
+                    <Button>
+                      <Link to={SIGN_UP_ROUTE}>SIGN UP</Link>
+                    </Button>
+                    <Button>
+                      <Link to={SIGN_IN_ROUTE}>Try for free</Link>
+                    </Button>
+                  </div>
+                </FlexCOntainer>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Container>
     </>
   );
 }
